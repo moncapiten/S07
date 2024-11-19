5c3bcd5e29220d6f825bf581fd4917f58231f2b8
moncapiten <moncapiten@gmail.com>
2024-11-19 10:13:43 +0100

    adding E133, fructose and sucrose to absorbance spectrum plot, changed graphs names

--- Code/matlab/elambda.m
+++ Code/matlab/elambda.m
@@ -5,6 +5,10 @@ ufl = [629.9471  611.6782  593.7855  526.7789  460.6394  399.2741];%nm
 gaml = [6.5112    7.8116    6.2143   11.4325    7.5963    5.3239 ];
 color = [ "red", "#ffa500", "#777777", "green", "#0027bd", "#a020f0"];
 
+E133 = readmatrix('../../Data/Extra/E133.csv');
+u133 = E133(:, 1);
+a133 = E133(:, 2);
+
 l_ = 1./ufl;
 
 %plot(l_(1:length(el)), el, 'o--', Color = '#0027BD');
@@ -16,6 +20,9 @@ for i = 1:length(el)
 end
 
 plot(ufl(1:length(el)), el, '--', Color = 'magenta');
+hold on
+plot(u133, a133, 'o', Color = 'black');
+
 
 
 names = ["Absorbance data", repelem("", length(el)-1), "Connecting Line"];
